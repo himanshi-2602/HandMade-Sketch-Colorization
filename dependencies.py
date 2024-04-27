913df216b21c76c7dd815a6273592120f1cd8929
himanshi-2602 <90631531+himanshi-2602@users.noreply.github.com>
2024-04-28 01:34:29 +0530

    Add files via upload

---
+++ dependencies.py
@@ -0,0 +1,20 @@
+'''
+file: dependencies.py
+author: @Vincit0re
+objective: Set all the dependencies for the project. This file contains all the import statements for all the libraries that are required in the project.
+date: 2023-03-09
+'''
+
+import cv2 as cv
+import numpy as np
+import copy
+import pandas as pd
+import os
+import sys
+import time
+import argparse
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split as tts
+import streamlit as st
+from PIL import Image
+import matplotlib.pyplot as plt
